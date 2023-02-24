81a51678ac7480faa8cb682e97e093ccc8df5c24
DNQuyTD <quytd@mcrew-tech.com>
2023-02-25 01:58:56 +0700

    Init structure

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+// init middlewares
+app.use(morgan("dev")) // To show request on console log ("combined", "common", "short", "tiny", "dev")
+app.use(helmet()) // Prevent third page read information from our page
+app.use(compression()) // Reduce size of input and output data
+
+
+// init db
+
+// init routes
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello Fantipjs'
+    return res.status(500).json({
+        message: 'Welcome to JS',
+        metadata: strCompress.repeat(10000)
+    })
+})
+
+// handling error
+
+module.exports = app
